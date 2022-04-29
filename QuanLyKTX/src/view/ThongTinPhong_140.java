9926cefc8dad0ded370bd0be064ac8261a036682
huynhHau <huynhhau2580@gmail.com>
2022-04-29 09:22:07 +0700

    Hau bo sung code form QLP lan 1

---
+++ QuanLyKTX/src/view/ThongTinPhong_140.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author Huynh Hau
+ */
+public class ThongTinPhong_140 {
+    private String tenPhong;
+    private String maPhong;
+    private String soSVHienTai;
+    private String soSVToiDa;
+    private String tinhTrangPhong;
+    private int loaiPhong;
+
+    
+    
+    
+    public void ThongTinPhong() {
+        
+    }
+    
+    public int getLoaiPhong() {
+        return loaiPhong;
+    }
+
+    public void setLoaiPhong(int loaiPhong) {
+        this.loaiPhong = loaiPhong;
+    }
+
+   
+    public String getTenPhong() {
+        return tenPhong;
+    }
+
+    public void setTenPhong(String tenPhong) {
+        this.tenPhong = tenPhong;
+    }
+
+    public String getMaPhong() {
+        return maPhong;
+    }
+
+    public void setMaPhong(String maPhong) {
+        this.maPhong = maPhong;
+    }
+
+    public String getSoSVHienTai() {
+        return soSVHienTai;
+    }
+
+    public void setSoSVHienTai(String soSVHienTai) {
+        this.soSVHienTai = soSVHienTai;
+    }
+
+    public String getSoSVToiDa() {
+        return soSVToiDa;
+    }
+
+    public void setSoSVToiDa(String soSVToiDa) {
+        this.soSVToiDa = soSVToiDa;
+    }
+
+    public String getTinhTrangPhong() {
+        return tinhTrangPhong;
+    }
+
+    public void setTinhTrangPhong(String tinhTrangPhong) {
+        this.tinhTrangPhong = tinhTrangPhong;
+    }
+    
+
+}
+    
