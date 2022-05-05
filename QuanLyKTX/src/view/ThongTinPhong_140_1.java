17e2979d309aa2056afcb1b9944f673979b00b69
huynhHau <huynhhau2580@gmail.com>
2022-05-05 21:52:13 +0700

    Hai Hau commit form quan Ly phong lan 8

---
+++ QuanLyKTX/src/view/ThongTinPhong_140_1.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author Huynh Hau
+ */
+public class ThongTinPhong_140_1 {
+    private String tenPhong;
+    private String maPhong;
+    private String soSVHienTai;
+    private String soSVToiDa;
+    private String tinhTrangPhong;
+    private int loaiPhong;
+
+    private String maSV;
+    private String maKTX;
+    private String hoTen;
+    private String CMND;
+    private String Sdt;
+    
+    public void ThongTinPhong() {
+        
+    }
+
+    public int getLoaiPhong() {
+        return loaiPhong;
+    }
+
+    public void setLoaiPhong(int loaiPhong) {
+        this.loaiPhong = loaiPhong;
+    }
+
+   
+    public String getTenPhong() {
+        return tenPhong;
+    }
+
+    public void setTenPhong(String tenPhong) {
+        this.tenPhong = tenPhong;
+    }
+
+    public String getMaPhong() {
+        return maPhong;
+    }
+
+    public void setMaPhong(String maPhong) {
+        this.maPhong = maPhong;
+    }
+
+    public String getSoSVHienTai() {
+        return soSVHienTai;
+    }
+
+    public void setSoSVHienTai(String soSVHienTai) {
+        this.soSVHienTai = soSVHienTai;
+    }
+
+    public String getSoSVToiDa() {
+        return soSVToiDa;
+    }
+
+    public void setSoSVToiDa(String soSVToiDa) {
+        this.soSVToiDa = soSVToiDa;
+    }
+
+    public String getTinhTrangPhong() {
+        return tinhTrangPhong;
+    }
+
+    public void setTinhTrangPhong(String tinhTrangPhong) {
+        this.tinhTrangPhong = tinhTrangPhong;
+    }
+    
+    public String getMaSV() {
+        return maSV;
+    }
+
+    public void setMaSV(String maSV) {
+        this.maSV = maSV;
+    }
+
+    public String getMaKTX() {
+        return maKTX;
+    }
+
+    public void setMaKTX(String maKTX) {
+        this.maKTX = maKTX;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getCMND() {
+        return CMND;
+    }
+
+    public void setCMND(String CMND) {
+        this.CMND = CMND;
+    }
+
+    public String getSdt() {
+        return Sdt;
+    }
+
+    public void setSdt(String Sdt) {
+        this.Sdt = Sdt;
+    }
+  
+
+}
+    
